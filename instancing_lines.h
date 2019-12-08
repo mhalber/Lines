9ee24cb961cf718588c9eb64f3b7bfb2cffac973
Maciej Halber <macikuh@gmail.com>
2019-12-08 01:13:36 -0800

    Added instance-based line rendering path

---
+++ instancing_lines.h
@@ -0,0 +1,223 @@
+#ifndef INSTANCING_LINES_H
+#define INSTANCING_LINES_H
+
+void* instancing_lines_init_device( void );
+uint32_t instancing_lines_update( void* device, const void* data, int32_t n_elems, int32_t elem_size );
+void instancing_lines_render( const void* device, const int32_t count, const float* mvp );
+
+//TODO(maciej): Terminate device!
+
+#endif /* INSTANCING_LINES_H */
+
+#ifdef INSTANCING_LINES_IMPLEMENTATION
+
+typedef struct instancing_lines_device
+{
+  GLuint program_id;
+  GLuint uniform_mvp_location;
+  GLuint attrib_quad_pos_location;
+  GLuint attrib_pos_0_location;
+  GLuint attrib_col_0_location;
+  GLuint attrib_pos_1_location;
+  GLuint attrib_col_1_location;
+  GLuint vao;
+  GLuint line_vbo;
+  GLuint quad_vbo;
+  GLuint quad_ebo;
+} instancing_lines_device_t;
+
+
+// TODO(maciej): Maybe move into a little gl_utils.h file.
+void
+instancing_lines_assert_shader_compiled( GLuint shader_id )
+{
+  GLint status;
+  glGetShaderiv( shader_id, GL_COMPILE_STATUS, &status );
+  if( status == GL_FALSE )
+  {
+    int32_t info_len = 0;
+    glGetShaderiv( shader_id, GL_INFO_LOG_LENGTH, &info_len );
+    GLchar* info = malloc( info_len );
+    glGetShaderInfoLog( shader_id, info_len, &info_len, info );
+    fprintf( stderr, "[GL] Compile error: \n%s\n", info );
+    free( info );
+    exit( -1 );
+  }
+}
+
+void
+instancing_lines_assert_program_linked( GLuint program_id )
+{
+  GLint status;
+  glGetProgramiv( program_id, GL_LINK_STATUS, &status );
+  if( status == GL_FALSE )
+  {
+    int32_t info_len = 0;
+    glGetProgramiv( program_id, GL_INFO_LOG_LENGTH, &info_len );
+    GLchar* info = malloc( info_len );
+    glGetProgramInfoLog( program_id, info_len, &info_len, info );
+    fprintf( stderr, "[GL] Link error: \n%s\n", info );
+    free( info );
+    exit(-1);
+  }
+}
+
+void
+instancing_lines_create_shader_program( instancing_lines_device_t* device )
+{
+  const char* vs_src = 
+    SHDR_VERSION
+    SHDR_SOURCE(
+      layout(location = 0) in vec3 quad_pos;
+      layout(location = 1) in vec3 line_pos_0;
+      layout(location = 2) in vec3 line_col_0;
+      layout(location = 3) in vec3 line_pos_1;
+      layout(location = 4) in vec3 line_col_1;
+      
+      layout(location = 0) uniform mat4 u_mvp;
+
+      out vec3 v_col;
+
+      void main()
+      {
+        vec3 colors[2] = vec3[2]( line_col_0, line_col_1 );
+        v_col = colors[ int(quad_pos.x) ];
+
+        float line_width = 0.025f;
+
+        vec3 u = line_pos_1 - line_pos_0;
+        vec3 d = normalize(u);
+        vec3 v = vec3(-d.y, d.x, d.z );
+
+        vec3 pos = line_pos_0 + quad_pos.x * u + quad_pos.y * v * line_width;
+
+        gl_Position = u_mvp * vec4( pos, 1.0 );
+      }
+    );
+  
+  const char* fs_src = 
+    SHDR_VERSION
+    SHDR_SOURCE(
+      in vec3 v_col;
+      out vec4 frag_color;
+      void main()
+      {
+        frag_color = vec4(v_col, 1.0);
+      }
+    );
+
+  GLuint vertex_shader   = glCreateShader( GL_VERTEX_SHADER );
+  GLuint fragment_shader = glCreateShader( GL_FRAGMENT_SHADER );
+
+  glShaderSource( vertex_shader, 1, &vs_src, 0 );
+  glCompileShader( vertex_shader );
+  instancing_lines_assert_shader_compiled( vertex_shader );
+
+  glShaderSource( fragment_shader, 1, &fs_src, 0 );
+  glCompileShader( fragment_shader );
+  instancing_lines_assert_shader_compiled( fragment_shader );
+
+
+  device->program_id = glCreateProgram();
+  glAttachShader( device->program_id, vertex_shader );
+  glAttachShader( device->program_id, fragment_shader );
+  glLinkProgram( device->program_id );
+  instancing_lines_assert_program_linked( device->program_id );
+
+  glDetachShader( device->program_id, vertex_shader );
+  glDetachShader( device->program_id, fragment_shader );
+  glDeleteShader( vertex_shader );
+  glDeleteShader( fragment_shader );
+
+  device->attrib_quad_pos_location = glGetAttribLocation( device->program_id, "quad_pos" );
+  device->attrib_pos_0_location = glGetAttribLocation( device->program_id, "line_pos_0" );
+  device->attrib_col_0_location = glGetAttribLocation( device->program_id, "line_col_0" );
+  device->attrib_pos_1_location = glGetAttribLocation( device->program_id, "line_pos_1" );
+  device->attrib_col_1_location = glGetAttribLocation( device->program_id, "line_col_1" );
+
+  device->uniform_mvp_location = glGetUniformLocation( device->program_id, "u_mvp" );
+}
+
+void
+instancing_lines_setup_geometry_storage( instancing_lines_device_t* device )
+{
+  GLuint  stream_idx = 0;
+  glCreateVertexArrays( 1, &device->vao );
+  glCreateBuffers( 1, &device->line_vbo );
+  glNamedBufferStorage( device->line_vbo, MAX_VERTS * sizeof(vertex_t), NULL, GL_DYNAMIC_STORAGE_BIT );
+
+  glVertexArrayVertexBuffer( device->vao, stream_idx, device->line_vbo, 0, 2 * sizeof(vertex_t) );
+  glVertexArrayBindingDivisor( device->vao, stream_idx, 1 );
+
+  glEnableVertexArrayAttrib( device->vao, device->attrib_pos_0_location );
+  glEnableVertexArrayAttrib( device->vao, device->attrib_col_0_location );
+  glEnableVertexArrayAttrib( device->vao, device->attrib_pos_1_location );
+  glEnableVertexArrayAttrib( device->vao, device->attrib_col_1_location );
+
+  glVertexArrayAttribFormat( device->vao, device->attrib_pos_0_location, 3, GL_FLOAT, GL_FALSE, offsetof(vertex_t, pos) );
+  glVertexArrayAttribFormat( device->vao, device->attrib_col_0_location, 3, GL_FLOAT, GL_FALSE, offsetof(vertex_t, col) );
+  glVertexArrayAttribFormat( device->vao, device->attrib_pos_1_location, 3, GL_FLOAT, GL_FALSE, sizeof(vertex_t) + offsetof(vertex_t, pos) );
+  glVertexArrayAttribFormat( device->vao, device->attrib_col_1_location, 3, GL_FLOAT, GL_FALSE, sizeof(vertex_t) + offsetof(vertex_t, col) );
+
+  glVertexArrayAttribBinding( device->vao, device->attrib_pos_0_location, stream_idx );
+  glVertexArrayAttribBinding( device->vao, device->attrib_col_0_location, stream_idx );
+  glVertexArrayAttribBinding( device->vao, device->attrib_pos_1_location, stream_idx );
+  glVertexArrayAttribBinding( device->vao, device->attrib_col_1_location, stream_idx );
+
+
+  stream_idx++;
+
+  float quad[] = {  0.0, -0.5, 0.0,
+                    0.0,  0.5, 0.0,
+                    1.0,  0.5, 0.0,
+                    1.0, -0.5, 0.0  };
+  uint16_t ind[] = { 0, 1, 2,  0, 2, 3 };
+  
+  glCreateBuffers( 1, &device->quad_vbo );
+  glCreateBuffers( 1, &device->quad_ebo );
+
+  glNamedBufferStorage( device->quad_vbo, 12*sizeof(float), quad, GL_DYNAMIC_STORAGE_BIT );
+  glNamedBufferStorage( device->quad_ebo, 6*sizeof(uint16_t), ind, GL_DYNAMIC_STORAGE_BIT );
+
+  glVertexArrayVertexBuffer( device->vao, stream_idx, device->quad_vbo, 0, 3*sizeof(float) );
+  glVertexArrayElementBuffer( device->vao, device->quad_ebo );
+
+  glEnableVertexArrayAttrib( device->vao, device->attrib_quad_pos_location );
+  glVertexArrayAttribFormat( device->vao, device->attrib_quad_pos_location, 3, GL_FLOAT, GL_FALSE, 0 );
+  glVertexArrayAttribBinding( device->vao, device->attrib_quad_pos_location, stream_idx );
+}
+
+void*
+instancing_lines_init_device( void )
+{
+  instancing_lines_device_t* device = malloc( sizeof(instancing_lines_device_t) );
+  memset( device, 0, sizeof(instancing_lines_device_t) );
+  instancing_lines_create_shader_program( device );
+  instancing_lines_setup_geometry_storage( device );
+  return device;
+}
+
+uint32_t
+instancing_lines_update( void* device_in, const void* data, int32_t n_elems, int32_t elem_size )
+{
+  instancing_lines_device_t* device = device_in;
+  glNamedBufferSubData( device->line_vbo, 0, n_elems*elem_size, data );
+  return n_elems;
+}
+
+void
+instancing_lines_render( const void* device_in, const int32_t count, const float* mvp )
+{
+  const instancing_lines_device_t* device = device_in;
+  glUseProgram( device->program_id );
+  glUniformMatrix4fv( device->uniform_mvp_location, 1, GL_FALSE, mvp );
+
+  glBindVertexArray( device->vao );
+
+  glDrawElementsInstanced( GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, NULL, count / 2 );
+
+  glBindVertexArray( 0 );
+  glUseProgram( 0 );
+}
+
+#endif /*INSTANCING_LINES_IMPLEMENTATION*/
--- main.c
+++ main.c
@@ -40,26 +40,26 @@ typedef struct vertex
 #define CPU_LINES_IMPLEMENTATION
 #define GEOMETRY_SHADER_LINES_IMPLEMENTATION
 #define TEX_BUFFER_LINES_IMPLEMENTATION
+#define INSTANCING_LINES_IMPLEMENTATION
 #include "gl_lines.h"
 #include "cpu_lines.h"
 #include "geometry_shader_lines.h"
 #include "tex_buffer_lines.h"
 // #include "vp_shift_lines.h"
-// #include "instancing_lines.h"
+#include "instancing_lines.h"
 
 typedef struct line_draw_engine
 {
-  void* device;
-  void* (*init_device)( void );
-  uint32_t (*update)( void*, const void*, int32_t, int32_t );
-  void (*render)( const void*, const int32_t, const float* );
+  void *device;
+  void *(*init_device)(void);
+  uint32_t (*update)(void *, const void *, int32_t, int32_t);
+  void (*render)(const void *, const int32_t, const float *);
 } line_draw_engine_t;
 
-void
-setup( line_draw_engine_t* engine, 
-       void* (*init_device_ptr)(void),
-       uint32_t (*update_ptr)( void*, const void*, int32_t, int32_t ),
-       void (*render_ptr)( const void*, const int32_t, const float* ) )
+void setup(line_draw_engine_t *engine,
+           void *(*init_device_ptr)(void),
+           uint32_t (*update_ptr)(void *, const void *, int32_t, int32_t),
+           void (*render_ptr)(const void *, const int32_t, const float *))
 {
   engine->init_device = init_device_ptr;
   engine->update = update_ptr;
@@ -68,21 +68,20 @@ setup( line_draw_engine_t* engine,
 }
 
 uint32_t
-update( line_draw_engine_t* engine, const void* data, int32_t n_elems, int32_t elem_size )
+update(line_draw_engine_t *engine, const void *data, int32_t n_elems, int32_t elem_size)
 {
-  return engine->update( engine->device, data, n_elems, elem_size );
+  return engine->update(engine->device, data, n_elems, elem_size);
 }
 
-void 
-render( line_draw_engine_t* engine, const int32_t count, const float* mvp )
+void render(line_draw_engine_t *engine, const int32_t count, const float *mvp)
 {
-  return engine->render( engine->device, count, mvp );
+  return engine->render(engine->device, count, mvp);
 }
 
-void generate_line_data( vertex_t* line_buf, uint32_t* line_buf_len, uint32_t line_buf_cap )
+void generate_line_data(vertex_t *line_buf, uint32_t *line_buf_len, uint32_t line_buf_cap)
 {
-  vertex_t* dst1 = line_buf;
-  vertex_t* dst2 = line_buf + 1;
+  vertex_t *dst1 = line_buf;
+  vertex_t *dst2 = line_buf + 1;
 
   int32_t grid_w = 100;
   int32_t grid_h = 100;
@@ -92,120 +91,128 @@ void generate_line_data( vertex_t* line_buf, uint32_t* line_buf_len, uint32_t li
   float d_theta = MSH_TWO_PI / circle_res;
   float radius = 0.04f;
 
-  for( int32_t iy = -grid_h/2; iy <= grid_h/2; iy++ )
+  for (int32_t iy = -grid_h / 2; iy <= grid_h / 2; iy++)
   {
     float cy = iy * grid_step;
-    for( int32_t ix = -grid_w/2; ix <= grid_w/2; ix++ )
+    for (int32_t ix = -grid_w / 2; ix <= grid_w / 2; ix++)
     {
       float cx = ix * grid_step;
-      msh_vec3_t prev_pos = msh_vec3( cx + radius*sin(0.0f), cy + radius*cos(0.0f), 0.0f );
+      msh_vec3_t prev_pos = msh_vec3(cx + radius * sin(0.0f), cy + radius * cos(0.0f), 0.0f);
       float x = 0.0f;
       float y = 0.0f;
-      for( int i = 1; i <= circle_res; ++i )
+      for (int i = 1; i <= circle_res; ++i)
       {
-        x = cx + radius * sin( i * d_theta );
-        y = cy + radius * cos( i * d_theta );
+        x = cx + radius * sin(i * d_theta);
+        y = cy + radius * cos(i * d_theta);
 
         dst1->pos = prev_pos;
-        dst2->pos = msh_vec3( x, y, 0 );
+        dst2->pos = msh_vec3(x, y, 0);
         prev_pos = dst2->pos;
-        dst1->col = msh_vec3( 0.9, 0.9, 0.9 );
-        dst2->col = msh_vec3( 0.9, 0.9, 0.9 );
-        dst1+=2;
-        dst2+=2;
+        dst1->col = msh_vec3(0.9, 0.9, 0.9);
+        dst2->col = msh_vec3(0.9, 0.9, 0.9);
+        dst1 += 2;
+        dst2 += 2;
         *line_buf_len += 2;
-        if( *line_buf_len >= line_buf_cap ) { printf( "[Spiral] Out of space!\n" ); break; }
+        if (*line_buf_len >= line_buf_cap)
+        {
+          printf("[Spiral] Out of space!\n");
+          break;
+        }
       }
     }
   }
 }
 
 int32_t
-main( int32_t argc, char** argv )
+main(int32_t argc, char **argv)
 {
   int32_t error = 0;
   error = !glfwInit();
-  if( error )
+  if (error)
   {
-    fprintf( stderr, "[] Failed to initialize glfw!\n" ); 
+    fprintf(stderr, "[] Failed to initialize glfw!\n");
     return EXIT_FAILURE;
   }
 
-  int32_t window_width=1024, window_height=1024;
-  glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 4 );
-  glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 5 );
-  glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
-  glfwWindowHint( GLFW_DOUBLEBUFFER, GL_FALSE );
-  GLFWwindow* window = glfwCreateWindow( window_width, window_height, "OGL Lines", NULL, NULL );
-  if( !window )
+  int32_t window_width = 1024, window_height = 1024;
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_DOUBLEBUFFER, GL_FALSE);
+  GLFWwindow *window = glfwCreateWindow(window_width, window_height, "OGL Lines", NULL, NULL);
+  if (!window)
   {
-    fprintf( stderr, "[] Failed to create window!\n" ); 
+    fprintf(stderr, "[] Failed to create window!\n");
     return EXIT_FAILURE;
   }
 
-  glfwMakeContextCurrent( window );
-  if( !gladLoadGLLoader( (GLADloadproc)glfwGetProcAddress ) ) { return EXIT_FAILURE; }
+  glfwMakeContextCurrent(window);
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    return EXIT_FAILURE;
+  }
 
   uint32_t line_buf_cap = MAX_VERTS / 3;
   uint32_t line_buf_len;
-  vertex_t* line_buf = malloc( line_buf_cap * sizeof(vertex_t) );
+  vertex_t *line_buf = malloc(line_buf_cap * sizeof(vertex_t));
 
-  uint32_t active_idx = 3;
+  uint32_t active_idx = 4;
   line_draw_engine_t engines[6] = {0};
-  setup( engines + 0, &gl_lines_init_device, &gl_lines_update, &gl_lines_render );
-  setup( engines + 1, &cpu_lines_init_device, &cpu_lines_update, &cpu_lines_render );
-  setup( engines + 2, &geom_shdr_lines_init_device, &geom_shdr_lines_update, &geom_shdr_lines_render );
-  setup( engines + 3, &tex_buffer_lines_init_device, &tex_buffer_lines_update, &tex_buffer_lines_render );
+  setup(engines + 0, &gl_lines_init_device, &gl_lines_update, &gl_lines_render);
+  setup(engines + 1, &cpu_lines_init_device, &cpu_lines_update, &cpu_lines_render);
+  setup(engines + 2, &geom_shdr_lines_init_device, &geom_shdr_lines_update, &geom_shdr_lines_render);
+  setup(engines + 3, &tex_buffer_lines_init_device, &tex_buffer_lines_update, &tex_buffer_lines_render);
+  setup(engines + 4, &instancing_lines_init_device, &instancing_lines_update, &instancing_lines_render);
 
   msh_camera_t cam = {0};
-  msh_camera_init( &cam, &(msh_camera_desc_t){ .eye = msh_vec3( 0.0f, 0.0f, 5.0f),
-                                               .center = msh_vec3_zeros(),
-                                               .up = msh_vec3_posy(),
-                                               .viewport = msh_vec4( 0.0f, 0.0f, window_width, window_height ),
-                                               .fovy = msh_rad2deg( 60 ),
-                                               .znear = 0.01f,
-                                               .zfar = 10.0f,
-                                               .use_ortho = true } );
-  msh_mat4_t mvp = msh_mat4_mul( cam.proj, cam.view );
+  msh_camera_init(&cam, &(msh_camera_desc_t){.eye = msh_vec3(0.0f, 0.0f, 5.0f),
+                                             .center = msh_vec3_zeros(),
+                                             .up = msh_vec3_posy(),
+                                             .viewport = msh_vec4(0.0f, 0.0f, window_width, window_height),
+                                             .fovy = msh_rad2deg(60),
+                                             .znear = 0.01f,
+                                             .zfar = 10.0f,
+                                             .use_ortho = true});
+  msh_mat4_t mvp = msh_mat4_mul(cam.proj, cam.view);
 
   uint64_t t1, t2;
-  while( !glfwWindowShouldClose( window ) )
+  while (!glfwWindowShouldClose(window))
   {
     // Update the camera
-    glfwGetWindowSize( window, &window_width, &window_height );
-    if( window_width != cam.viewport.z || window_height != cam.viewport.w )
+    glfwGetWindowSize(window, &window_width, &window_height);
+    if (window_width != cam.viewport.z || window_height != cam.viewport.w)
     {
       cam.viewport.z = window_width;
       cam.viewport.w = window_height;
-      msh_camera_update_proj( &cam );
-      mvp = msh_mat4_mul( cam.proj, cam.view );
+      msh_camera_update_proj(&cam);
+      mvp = msh_mat4_mul(cam.proj, cam.view);
     }
 
     t1 = msh_time_now();
     line_buf_len = 0;
-    generate_line_data( line_buf, &line_buf_len, line_buf_cap );
+    generate_line_data(line_buf, &line_buf_len, line_buf_cap);
     t2 = msh_time_now();
-    double diff1 = msh_time_diff_ms( t2, t1 );
+    double diff1 = msh_time_diff_ms(t2, t1);
 
-    glClearColor( 0.12f, 0.12f, 0.12f, 1.0f );
-    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+    glClearColor(0.12f, 0.12f, 0.12f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glViewport(0, 0, window_width, window_height);
 
     t1 = msh_time_now();
 
-    line_draw_engine_t* active_engine = engines + active_idx;
-    uint32_t elem_count = update( active_engine, line_buf, line_buf_len, sizeof(vertex_t) );
-    render( active_engine, elem_count, &mvp.data[0] );
+    line_draw_engine_t *active_engine = engines + active_idx;
+    uint32_t elem_count = update(active_engine, line_buf, line_buf_len, sizeof(vertex_t));
+    render(active_engine, elem_count, &mvp.data[0]);
 
     glFlush();
     glfwPollEvents();
-    
+
     t2 = msh_time_now();
-    double diff2 = msh_time_diff_ms( t2, t1 );
+    double diff2 = msh_time_diff_ms(t2, t1);
 
     char name[128] = {0};
-    snprintf( name, 128, "Lines - %6.4fms - %6.4fms", diff1, diff2 );
-    glfwSetWindowTitle( window, name );
+    snprintf(name, 128, "Lines - %6.4fms - %6.4fms", diff1, diff2);
+    glfwSetWindowTitle(window, name);
   }
 
   glfwTerminate();
